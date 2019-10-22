e42dd525a371d0a905f571edb90eef12339b8f57
Akash Khan <akash@Akashs-iMac.local>
2019-10-22 16:14:46 +0600

    Initial Commit

---
+++ ImageLoad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLoad
+//
+//  Created by Akash Khan on 10/22/19.
+//  Copyright © 2019 Akash Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
